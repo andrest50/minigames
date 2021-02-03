a8ea948cd4e3677e3304854d7e5003687b9b691f
Andres Tobon <andrest2455@gmail.com>
2021-02-02 22:57:12 -0800

    Simplified memory game code and fixed some bugs.

--- MemoryGame/MemoryGame.cpp
+++ MemoryGame/MemoryGame.cpp
@@ -4,21 +4,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
+//function prototypes
+void mainMenu();
 int getDifficulty();
 int getInitialNumbers();
-void runEasy(vector<int>, int);
-void runMedium(vector<int>, int);
-void runHard(vector<int>, int);
-void play();
+void gameOver(vector<int>, vector<int>);
+void runGame(vector<int>, int, int);
 
-void play(){
+void mainMenu(){
 	int option = 1;
 	int difficulty = 0;
 	int numNumbers = 0;
+	int digits = 0;
 	vector<int> numbers;
+
 	while(option != 2){
 		cout << "Welcome to the Memory Game!" << endl;
 		cout << "(1) Play - (2) Quit" << endl;
@@ -29,17 +32,19 @@ void play(){
 
 		difficulty = getDifficulty();
 		numNumbers = getInitialNumbers();
+		numbers.clear();
 		switch (difficulty){
 			case 1:
-				runEasy(numbers, numNumbers);
+				digits = 1;
 				break;
 			case 2:
-				runMedium(numbers, numNumbers);
+				digits = 2;
 				break;
 			case 3:
-				runHard(numbers, numNumbers);
+				digits = 3;
 				break;
 		}
+		runGame(numbers, numNumbers, digits);
 	}
 }
 
@@ -55,80 +60,82 @@ int getDifficulty(){
 }
 
 int getInitialNumbers(){
-	int numbers;
-	cout << "How many numbers do you want to start with? (will increment by 1 each time)" << endl;
-	cin >> numbers;
+	int numbers = 0;
+	while(numbers > 10 || numbers < 1){
+		cout << "How many numbers do you want to start with? (will increment by 1 each time)" << endl;
+		cin >> numbers;
+	}
 	return numbers;
 }
 
-void runEasy(vector<int> numbers, int numNumbers){
-	int option = 1;
-	int lose = 0;
-	int value = 0;
+void gameOver(vector<int> response, vector<int> answer){
+	int numNumbers = response.size();
 
-	while (option != 2) {
-		for (int i = numNumbers - 1; i >= 0; i--){
-			value = rand() % 9 + 1;
-			numbers.push_back(value);
-			cout << value;
-			cout << " ";
-			if(i == 0)
-				sleep(3);
-		}
-		for(int i = 0; i < 50; i++){
-			cout << endl;
-		}
-		cout << "\nDo you want to continue onto the next level?" << endl;
-		cout << "(1) Yes - (2) No" << endl;
-		cin >> option;
-		numNumbers++;
-		numbers.clear();
+	cout << "----------------------------------------------" << endl;
+
+	//print response
+	cout << "Response: \t";
+	for(int i = 0; i < numNumbers; i++){
+		cout << response[i] << " ";
 	}
+	cout << endl;
+
+	//print answer
+	cout << "Answer: \t";
+	for(int i = 0; i < numNumbers; i++){
+		cout << answer[i] << " ";
+	}
+	cout << endl;
+
+	cout << "You made it to " << numNumbers << " numbers!" << endl;
+	cout << "----------------------------------------------" << endl;
 }
 
-void runMedium(vector<int> numbers, int numNumbers){
-	int option = 1;
-	int lose = 0;
-	int value;
+void runGame(vector<int> numbers, int numNumbers, int digits){
+	int active = 1; //1 - still in game, 0 - game over
+	int value = 0; //for getting random numbers
+	int multiplier = pow(10, digits-1); //for calculating numbers based on difficulty
+	int sleepTime = 3; //time in seconds
+	vector<int> response; //store response numbers
 
-	while (option != 2) {
+	while (active == 1) {
+		cout << "----------------------------------------------" << endl;
 		for (int i = numNumbers - 1; i >= 0; i--){
-			value = rand() % 90 + 10;
+				value = rand() % (9 * multiplier) + 1 * multiplier;
 			numbers.push_back(value);
 			cout << value;
 			cout << " ";
 		}
-		cout << "\nDo you want to continue onto the next level?" << endl;
-		cout << "(1) Yes - (2) No" << endl;
-		cin >> option;
-		numNumbers++;
-		numbers.clear();
-	}
-}
+		cout.flush();
+		sleep(sleepTime);
+		cout << "\rWhat was the set of numbers? (separate numbers with a space)" << endl;
 
-void runHard(vector<int> numbers, int numNumbers){
-	int option = 1;
-	int lose = 0;
-	int value = 0;
-
-	while (option != 2) {
-		for (int i = numNumbers - 1; i >= 0; i--){
-			value = rand() % 900 + 100;
-			numbers.push_back(value);
-			cout << value;
-			cout << " ";
+		//get the response
+		for(int i = 0; i < numNumbers; i++){
+			cin >> value;
+			response.push_back(value);
 		}
-		cout << "\nDo you want to continue onto the next level?" << endl;
-		cout << "(1) Yes - (2) No" << endl;
-		cin >> option;
-		numNumbers++;
-		numbers.clear();
+
+		//check to see if the response was correct
+		for(int i = 0; i < numNumbers; i++){
+			if(numbers[i] != response[i])
+				active = 0;
+		}
+
+		if(active == 1){
+			numNumbers++;
+			sleepTime++;
+			numbers.clear();
+			response.clear();
+		}
 	}
+
+	gameOver(response, numbers);
 }
 
 int main()
 {
 	srand(time(NULL));
-	play();
+	mainMenu();
 	return 0;
 }
